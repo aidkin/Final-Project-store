0889c18649ddf49841be8c8c285d5b40de04fd4f
AidaS00 <72813226+AidaS00@users.noreply.github.com>
2021-05-27 12:13:14 +0600

    1

---
+++ lib/cookie_detail.dart
@@ -0,0 +1,120 @@
+import 'package:flutter/material.dart';
+import 'package:final_project/bottom_bar.dart';
+
+
+class CookieDetail extends StatelessWidget {
+  final assetPath, cookieprice, cookiename;
+
+  CookieDetail({this.assetPath, this.cookieprice, this.cookiename});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0.0,
+        centerTitle: true,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back, color: Color(0xFF545D68)),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
+        title: Text('Pickup',
+            style: TextStyle(
+                fontFamily: 'Varela',
+                fontSize: 20.0,
+                color: Color(0xFF545D68))),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.notifications_none, color: Color(0xFF545D68)),
+            onPressed: () {},
+          ),
+        ],
+      ),
+
+      body: ListView(
+          children: [
+            SizedBox(height: 15.0),
+            Padding(
+              padding: EdgeInsets.only(left: 20.0),
+              child: Text(
+                  'Gucci Card',
+                  style: TextStyle(
+                      fontFamily: 'Varela',
+                      fontSize: 42.0,
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xFF000000))
+              ),
+            ),
+            SizedBox(height: 15.0),
+            Hero(
+                tag: assetPath,
+                child: Image.asset(assetPath,
+                    height: 150.0,
+                    width: 100.0,
+                    fit: BoxFit.contain
+                )
+            ),
+            SizedBox(height: 20.0),
+            Center(
+              child: Text(cookieprice,
+                  style: TextStyle(
+                      fontFamily: 'Varela',
+                      fontSize: 22.0,
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xFF000000))),
+            ),
+            SizedBox(height: 10.0),
+            Center(
+              child: Text(cookiename,
+                  style: TextStyle(
+                      color: Color(0xFF000000),
+                      fontFamily: 'Varela',
+                      fontSize: 24.0)),
+            ),
+            SizedBox(height: 20.0),
+            Center(
+              child: Container(
+                width: MediaQuery.of(context).size.width - 50.0,
+                child: Text('Very beautiful bags, for women, men and kids',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        fontFamily: 'Varela',
+                        fontSize: 16.0,
+                        color: Color(0xFFB4B8B9))
+                ),
+              ),
+            ),
+            SizedBox(height: 20.0),
+            Center(
+                child: Container(
+                    width: MediaQuery.of(context).size.width - 50.0,
+                    height: 50.0,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(25.0),
+                        color: Color(0xFFB39DDB)
+                    ),
+                    child: Center(
+                        child: Text('Add to cart',
+                          style: TextStyle(
+                              fontFamily: 'Varela',
+                              fontSize: 14.0,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white
+                          ),
+                        )
+                    )
+                )
+            )
+          ]
+      ),
+
+      floatingActionButton: FloatingActionButton(onPressed: () {},
+        backgroundColor: Color(0xFF676E79),
+        child: Icon(Icons.fastfood),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: BottomBar(),
+    );
+  }
+}
